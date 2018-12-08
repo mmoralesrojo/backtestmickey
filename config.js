4c83e6a55ad14ea59e0cb1709251c4ed04d0442a
mmoralesrojo <mmoralesrojo0901@gmail.com>
2018-12-08 16:47:43 -0500

    Agregar config

---
+++ config.js
@@ -0,0 +1,36 @@
+module.exports = {
+	server: {
+		host: '0.0.0.0',
+		port: 3000,
+		baseUrl: '/'
+	},
+
+	odm: {
+		kamigames: {
+			db: {
+				client: 'mongo',
+				serviceName: 'mongodb',
+				connection: {
+					database: 'kamigames',
+					host: 'ec2-18-191-78-22.us-east-2.compute.amazonaws.com',
+					port: 27017,
+					user: 'kguser',
+					password: '1234'
+				}
+			}
+		}
+		kidsbackyard: {
+			db: {
+				client: 'mongo',
+				serviceName: 'mongodb',
+				connection: {
+					database: 'kidsbackyard',
+					host: 'ec2-18-191-78-22.us-east-2.compute.amazonaws.com',
+					port: 27017,
+					user: 'kguser',
+					password: '1234'
+				}
+			}
+		}
+	}
+}
